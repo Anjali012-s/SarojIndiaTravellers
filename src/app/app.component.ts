c71595361915dcfb3aee10ebc9e9a3b4e466868c
gagankumar03 <gagankumar03@nagarro.com>
2024-05-11 09:54:12 +0530

    contact form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HomePageComponent} from '../app/home-page/home-page.component';
+import { HomePageComponent } from '../app/home-page/home-page.component';
+import { ContactComponent } from './components/contact/contact.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomePageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, HomePageComponent, ContactComponent],
 })
 export class AppComponent {
   title = 'SarojIndiaTravellers';
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeSliderComponent } from './home-slider/home-slider.component';
 import { ExperiencesComponent } from '../experiences/experiences.component';
+import { ContactComponent } from '../components/contact/contact.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HomeSliderComponent,ExperiencesComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
+  imports: [HomeSliderComponent, ExperiencesComponent, ContactComponent],
 })
 export class HomePageComponent implements OnInit {
-  imageSlides:any[] = new Array(3).fill({id: -1,src:''});
+  imageSlides: any[] = new Array(3).fill({ id: -1, src: '' });
   ngOnInit(): void {
-    this.imageSlides[0]={
-      src:'/assets/images/img1.jpg'
+    this.imageSlides[0] = {
+      src: '/assets/images/img1.jpg',
     };
-    this.imageSlides[1]={
-      src:'/assets/images/img2.jpg'
+    this.imageSlides[1] = {
+      src: '/assets/images/img2.jpg',
     };
-    this.imageSlides[2]={
-      src:'/assets/images/img3.jpg'
+    this.imageSlides[2] = {
+      src: '/assets/images/img3.jpg',
     };
   }
 }
